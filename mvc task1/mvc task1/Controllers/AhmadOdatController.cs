cec8819f4b5608960e449e7ff6a07b866d63ee54
01-AhmadOdat <adodat91@gmail.com>
2023-01-31 23:21:59 +0300

    task1 in mvc

---
+++ mvc task1/mvc task1/Controllers/AhmadOdatController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_task1.Controllers
+{
+    public class AhmadOdatController : Controller
+    {
+        // GET: AhmadOdat
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Name()
+        {
+            return Content("<h1>Ahmad Odat<h1/>");
+        }
+
+        public ActionResult Age()
+        {
+            return Content("<h2>my age is 27</h2>");
+        }
+
+        public ActionResult Major()
+        {
+            return Content("<h2>Electrical Engineer</h2>");
+        }
+
+        public ActionResult Hoppies()
+        {
+            return Content("<head><link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC\" crossorigin=\"anonymous\">\r\n<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM\" crossorigin=\"anonymous\"></script></head><table class='table table-stripped'><tr><th>hopy name</th><th>my age</th><tr/>" +
+                "<tr><td>football</td><td>10 years</td><tr/>" +
+                "<tr><td>swimming</td><td>from 15-25 yers</td><tr/><table/>");
+        }
+    }
+}
---
+++ mvc task1/mvc task1/Controllers/DefaultController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_task1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        //public ActionResult Index()
+        //{
+        //    string x = "\"<img src=\\\".\\\\juce.png\\\" alt=\\\"\\\">\"";
+         //return ("<a href='../juce.png' download ><img src = '../juce.png' /></a>");
+        //    return View(x);
+        //}http://localhost:53093/
+        public string Index()
+        {
+
+            return ("<a href='../juce.png' download ><img src = '../juce.png' /></a>"+ "<br/><a href='../Views/Home/About.cshtml'><h1>About us<h1/></a>");
+            //return ("<img src = '../juce.png' />");
+        }
+
+        public string About()
+        {
+
+            return ("<h1>About us<h1/><br/><h2>My name is Ahmad Odat<h2/><br/><img src = '../ahmad.png' />");
+            
+        }
+
+        public string Contact()
+        {
+
+            return ("<a href='https://web.facebook.com/profile.php?id=100086789733640'><h1>Contact me<h1/></a>");
+
+        }
+    }
+}
