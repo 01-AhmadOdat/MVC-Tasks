61c1f17c0a45db68882fda0beebc2a909fed0e91
01-AhmadOdat <adodat91@gmail.com>
2023-02-01 17:07:29 +0300

    second task

---
+++ MVC task2/MVC task2/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using MVC_task2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_task2.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Models.Student>stu= new List<Models.Student>();
+            stu.Add(new Student {ID=1, Name= "Ahmad Odat", Major="Electrical engineering",Faculty="Engineering" });
+            stu.Add(new Student { ID = 2, Name = "Hassan", Major = "computer sience", Faculty = "IT" });
+
+            return View(stu);
+        }
+
+    }
+}
