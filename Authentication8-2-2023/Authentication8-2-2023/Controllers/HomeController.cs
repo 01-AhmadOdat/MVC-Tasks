8ecef13b561b61bcd404c416d6bfab1785fb9120
01-AhmadOdat <adodat91@gmail.com>
2023-02-08 22:02:10 +0300

    MVC 8/2/2023

---
+++ Authentication8-2-2023/Authentication8-2-2023/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Authentication8_2_2023.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Authentication8_2_2023.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        project5Entities db = new project5Entities();
+        public ActionResult Index()
+        {
+            var Amd = db.Products.ToList();
+            return View(Amd);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Authentication8-2-2023/Authentication8-2-2023/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Authentication8_2_2023.MvcApplication" Language="C#" %>
